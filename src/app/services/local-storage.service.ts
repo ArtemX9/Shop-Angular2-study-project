94937a2e458631e1818e0e2f649bba8c69973755
Trukhanov Artem <trukhanovartem@me.com>
2018-07-12 19:13:21 +0300

    Task3: Create LocalStorageService.

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+  private localStorage = window.localStorage;
+
+  constructor() { }
+
+  setItem(key: string, item: any) {
+    this.localStorage.setItem(key, JSON.stringify(item));
+  }
+
+  getItem(key: string): any {
+    const item = this.localStorage.getItem(key);
+    return JSON.parse(item);
+  }
+
+  removeItem(key: string) {
+    this.localStorage.removeItem(key);
+  }
+}
